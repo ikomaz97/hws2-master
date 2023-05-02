2b4afb843dc1149a28a79ee34bfe276e615028e1
ikomaz97 <ikomaz97@gmail.com>
2023-05-02 20:15:18 +0400

    02-05-03

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import s from './Message.module.css'
 import avatar from "../avatar.png"
+import {MessageType} from "../HW1";
 // нужно создать правильный тип вместо any
-export type Message = {
-    message: string,
-    id:number,
-}
-type MessagePropsType ={
-    message:Message,
 
+
+
+export type MessagePropsType = {
+    message: MessageType
 }
 
 // нужно отобразить приходящие данные
-const Message = (props: MessagePropsType) => {
+const Message =  (props: MessagePropsType) => {
     return (
         <div id={'hw1-message-' + props.message.id} className={s.message}>
             <div className={s.imageAndText}>
-                <img
+                <img src={avatar}
                     id={'hw1-avatar-' + props.message.id}
                     // создаёт студент
 
@@ -25,7 +24,7 @@ const Message = (props: MessagePropsType) => {
                 <div className={s.text}>
                     <div id={'hw1-name-' + props.message.id} className={s.name}>
                         {/*создаёт студент*/}
-
+                        {props.message.user.name}
                         {/**/}
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
