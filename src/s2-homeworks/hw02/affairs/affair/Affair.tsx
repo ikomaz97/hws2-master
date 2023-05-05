be34a500a7940353d98c578e977733fc3192a9c3
ikomaz97 <ikomaz97@gmail.com>
2023-05-05 12:12:51 +0400

    04/05/01

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -3,6 +3,11 @@ import { AffairType } from '../../HW2'
 import s from './Affair.module.css'
 import s2 from '../Affairs.module.css'
 
+type AffairType = {
+    priority: string
+    _id: number
+}
+
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
@@ -12,6 +17,13 @@ type AffairPropsType = {
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
         // need to fix
+        // пропс.функция(мне нужен _id)
+        // давайте проследим боевой путь это функции, или как она будет всплывать:
+        // открывай в нескольких окнах и следи:
+        // отсюда она всплывет в компоненту Affairs вместе с _id ->
+        // далее из Affairs всплывет в HW2->
+        // в HW2 находим deleteAffairCallback- это и есть наш клиент ->
+        // deleteAffairCallback вызовет setAffairs(...) и   deleteAffair(...)
     }
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
@@ -25,18 +37,20 @@ function Affair(props: AffairPropsType) {
         >
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
                 {/*создаёт студент*/}
-
+                {/* ПРОПС.ВЫВОДИМ ИМЯ*/}
                 {/**/}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
-                {props.affair.priority}
+                {/*создаёт студент*/}
+
+                {/**/}
             </div>
 
             <button
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
                 // need to fix
-
+                //ОНКЛИК={ФУНКЦИЯ}
             >
                 {/*текст кнопки могут изменить студенты*/}
                 X
